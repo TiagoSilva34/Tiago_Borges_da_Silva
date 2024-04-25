132bae1639eeac46f40584c19b6a921232877ad3
Tiago Silva <tiagoborges899@gmail.com>
2024-04-25 07:43:20 -0300

    readme.md e Productitem

--- app/components/Products/ProductItem/index.js
+++ app/components/Products/ProductItem/index.js
@@ -14,19 +14,19 @@ export const ProductItem = ({ product }) => {
     router.push("/detalhes");
   };
 
-  // useEffect(() => {
-  //   localStorage.removeItem('updateCampaign')
+  useEffect(() => {
+    localStorage.removeItem('updateCampaign')
 
-  //   setTimeout(() => {
-  //    let storageCampaign = JSON.parse(localStorage.getItem('campaignData'))
+    setTimeout(() => {
+     let storageCampaign = JSON.parse(localStorage.getItem('campaignData'))
 
-  //    storageCampaign.filter(item => {
-  //     if (item.productName === product.title) {
-  //      setCampaignInfo(item)
-  //     }
-  //    })
-  //   }, 1000)
-  // }, [getStorageData, product.title]);
+     storageCampaign.filter(item => {
+      if (item.productName === product.title) {
+       setCampaignInfo(item)
+      }
+     })
+    }, 1000)
+  }, [getStorageData, product.title]);
 
   return (
     <div className="productImage my-3 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 border-blue-400">
